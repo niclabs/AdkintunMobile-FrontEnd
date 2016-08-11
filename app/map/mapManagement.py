85c05dda8544c0c42beca8dddd605434fc3302dd
Patricio <phfollert@gmail.com>
2016-08-11 17:23:18 -0400

    Fixing filter by antenna and adding chart

--- app/map/mapManagement.py
+++ app/map/mapManagement.py
@@ -5,6 +5,7 @@ from app import db
 
 points = [8, 11]
 
+
 def build(newZoom, carrier, bounds):
     if carrier == 0:
         if newZoom <= 8:
@@ -22,36 +23,33 @@ def build(newZoom, carrier, bounds):
 
             return getData(query1, query2, type)
         else:
-            locations = []
-            query = "SELECT antennas.lat, antennas.lon, SUM(gsm_count.quantity) as quantity FROM public.antennas, public.gsm_count WHERE gsm_count.antenna_id = antennas.id AND antennas.lat > %r AND antennas.lon > %r AND antennas.lat < %r AND antennas.lon < %r GROUP BY antennas.id;" % (
-            bounds["sw"]["lat"], bounds["sw"]["lon"], bounds["ne"]["lat"], bounds["ne"]["lon"])
-            print(query)
-            result = db.engine.execute(query)
-            for row in result:
-                locations.append({"lat": row["lat"], "lon": row["lon"], "quantity": row["quantity"]})
-            return {"locations" : locations, "action": "cluster"}
+            type = "Antena"
+            query1 = "SELECT antennas.id as id, antennas.id as name, antennas.lat, antennas.lon, SUM(gsm_count.quantity) as quantity FROM public.antennas, public.gsm_count WHERE gsm_count.antenna_id = antennas.id AND antennas.lat > %r AND antennas.lon > %r AND antennas.lat < %r AND antennas.lon < %r GROUP BY antennas.id;" % (
+                bounds["sw"]["lat"], bounds["sw"]["lon"], bounds["ne"]["lat"], bounds["ne"]["lon"])
+            query2 = "SELECT antennas.id as id, gsm_count.network_type as type, SUM(gsm_count.quantity) as quantity FROM public.antennas, public.gsm_count WHERE gsm_count.antenna_id = antennas.id AND antennas.lat > %r AND antennas.lon > %r AND antennas.lat < %r AND antennas.lon < %r GROUP BY antennas.id, gsm_count.network_type;" % (
+                bounds["sw"]["lat"], bounds["sw"]["lon"], bounds["ne"]["lat"], bounds["ne"]["lon"])
+            return getData(query1, query2, type, "cluster")
+
     else:
         if newZoom <= 8:
             type = "Región"
-            query1 = "SELECT region.id, region.name, region.lat, region. lon, SUM(gsm_count.quantity) as quantity FROM public.region, public.gsm_count, public.antennas, public.carriers, public.city WHERE gsm_count.antenna_id = antennas.id AND antennas.city_id = city.id AND antennas.carrier_id = carriers.id AND carriers.id = %r AND city.region_id = region.id  GROUP BY region.id;" % carrier
+            query1 = "SELECT region.id, region.name, region.lat, region.lon, SUM(gsm_count.quantity) as quantity FROM public.region, public.gsm_count, public.antennas, public.city WHERE gsm_count.antenna_id = antennas.id AND antennas.city_id = city.id AND gsm_count.carrier_id = %r AND city.region_id = region.id  GROUP BY region.id;" % carrier
 
-            query2 = "SELECT region.id, gsm_count.network_type as type, SUM(gsm_count.quantity) as quantity FROM public.region, public.gsm_count, public.antennas, public.carriers, public.city WHERE gsm_count.antenna_id = antennas.id AND antennas.city_id = city.id AND antennas.carrier_id = carriers.id AND carriers.id = %r AND city.region_id = region.id GROUP BY region.id, gsm_count.network_type;" % carrier
+            query2 = "SELECT region.id, gsm_count.network_type as type, SUM(gsm_count.quantity) as quantity FROM public.region, public.gsm_count, public.antennas, public.city WHERE gsm_count.antenna_id = antennas.id AND antennas.city_id = city.id AND gsm_count.carrier_id = %r AND city.region_id = region.id GROUP BY region.id, gsm_count.network_type;" % carrier
             return getData(query1, query2, type)
         elif newZoom <= 11:
             type = "Ciudad"
-            query1 = "SELECT city.id, city.name, city.lat, city.lon, SUM(gsm_count.quantity) as quantity FROM public.gsm_count, public.antennas, public.carriers, public.city WHERE gsm_count.antenna_id = antennas.id AND antennas.carrier_id = carriers.id AND antennas.city_id = city.id AND carriers.id = %r AND city.lat IS NOT NULL AND city.lon IS NOT NULL GROUP BY city.id;" % carrier
+            query1 = "SELECT city.id, city.name, city.lat, city.lon, SUM(gsm_count.quantity) as quantity FROM public.gsm_count, public.antennas, public.city WHERE gsm_count.antenna_id = antennas.id AND antennas.city_id = city.id AND gsm_count.carrier_id = %r AND city.lat IS NOT NULL AND city.lon IS NOT NULL GROUP BY city.id;" % carrier
 
-            query2 = "SELECT city.id, gsm_count.network_type as type, SUM(gsm_count.quantity) as quantity FROM public.gsm_count, public.antennas, public.carriers, public.city WHERE gsm_count.antenna_id = antennas.id AND antennas.carrier_id = carriers.id AND antennas.city_id = city.id AND carriers.id = %r GROUP BY city.id, gsm_count.network_type;" % carrier
+            query2 = "SELECT city.id, gsm_count.network_type as type, SUM(gsm_count.quantity) as quantity FROM public.gsm_count, public.antennas, public.city WHERE gsm_count.antenna_id = antennas.id AND antennas.city_id = city.id AND gsm_count.carrier_id= %r GROUP BY city.id, gsm_count.network_type;" % carrier
             return getData(query1, query2, type)
         else:
-            locations = []
-            query = "SELECT antennas.lat, antennas.lon, SUM(gsm_count.quantity) as quantity FROM public.antennas, public.gsm_count WHERE gsm_count.antenna_id = antennas.id AND antennas.lat > %r AND antennas.lon > %r AND antennas.lat < %r AND antennas.lon < %r and antennas.carrier_id = %r GROUP BY antennas.id;" % (
-                bounds["sw"]["lat"], bounds["sw"]["lon"], bounds["ne"]["lat"], bounds["ne"]["lon"], carrier)
-            print(query)
-            result = db.engine.execute(query)
-            for row in result:
-                locations.append({"lat": row["lat"], "lon": row["lon"], "quantity": row["quantity"]})
-            return {"locations": locations, "action": "cluster"}
+            type = "Antena"
+            query1 = "SELECT antennas.id as id, antennas.id as name, antennas.lat, antennas.lon, SUM(gsm_count.quantity) as quantity FROM public.antennas, public.gsm_count WHERE gsm_count.antenna_id = antennas.id AND gsm_count.carrier_id = %r AND antennas.lat > %r AND antennas.lon > %r AND antennas.lat < %r AND antennas.lon < %r GROUP BY antennas.id;" % (
+                carrier, bounds["sw"]["lat"], bounds["sw"]["lon"], bounds["ne"]["lat"], bounds["ne"]["lon"])
+            query2 = "SELECT antennas.id as id, gsm_count.network_type as type, SUM(gsm_count.quantity) as quantity FROM public.antennas, public.gsm_count WHERE gsm_count.antenna_id = antennas.id AND gsm_count.carrier_id = %r AND antennas.lat > %r AND antennas.lon > %r AND antennas.lat < %r AND antennas.lon < %r GROUP BY antennas.id, gsm_count.network_type;" % (
+                carrier, bounds["sw"]["lat"], bounds["sw"]["lon"], bounds["ne"]["lat"], bounds["ne"]["lon"])
+            return getData(query1, query2, type, "cluster")
 
 
 def change(lastZoom, newZoom, lastCarrier, newCarrier, bounds):
@@ -64,7 +62,7 @@ def change(lastZoom, newZoom, lastCarrier, newCarrier, bounds):
     return {"action": "noChange"}
 
 
-def getData(sqlQuery, sqlQuery2, type):
+def getData(sqlQuery, sqlQuery2, type, action="change"):
     data = {}
     locations = {}
     query = sqlQuery
@@ -77,4 +75,4 @@ def getData(sqlQuery, sqlQuery2, type):
     result = db.engine.execute(query)
     for row in result:
         data[row["id"]][row["type"]] = row["quantity"]
-    return {"data": data, "locations": locations, "type": type, "action": "change"}
+    return {"data": data, "locations": locations, "type": type, "action": action}
