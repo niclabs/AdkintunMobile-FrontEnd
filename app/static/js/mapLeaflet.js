60803432b8299e30b09a5f968149abf5c6656edb
ErianSeph <cris.parra.o@gmail.com>
2017-01-11 16:27:40 -0300

    Changes: - Mouse over the antennas shows donut chart again - Fixed a bug in ranking graphs not showing correctly

--- app/static/js/mapLeaflet.js
+++ app/static/js/mapLeaflet.js
@@ -2,6 +2,47 @@ var map;
 var markerCluster;
 var markerList;
 
+function mouseOverEvent(dataAntennas) {
+    return function (e) {
+        console.log(JSON.stringify(dataAntennas, null, 4));
+        console.log(dataAntennas);
+
+        if (dataAntennas.Total === 0) {
+            $("#infoGraph").html(
+                "<br> <div class='text-center'> <b> No hay información disponible para mostrar </b> </div> </br>");
+        }
+        else {
+            $("#infoGraph").html(
+                "<div class='alert alert-success'> <strong>Cantidad de eventos para esta antena: </strong>" + dataAntennas.Total + "</div>" +
+                "<div id='antennaChart'></div>");
+            var chart = c3.generate({
+                bindto: '#antennaChart',
+                data: {
+                    columns: [
+                        ['2G', dataAntennas["2G"]],
+                        ['3G', dataAntennas["3G"]],
+                        ['4G', dataAntennas["4G"]],
+                        ['Otras', dataAntennas.Otras]
+                    ],
+                    type: 'donut',
+                    onclick: function (d, i) {
+                        console.log("onclick", d, i);
+                    },
+                    onmouseover: function (d, i) {
+                        console.log("onmouseover", d, i);
+                    },
+                    onmouseout: function (d, i) {
+                        console.log("onmouseout", d, i);
+                    },
+                },
+                donut: {
+                    title: "Distribución de redes"
+                }
+            })
+        }
+    };
+}
+
 $(document).ready(function initmap() {
     map = L.map('map');
     var Santiago = [-33.447487, -70.673676];
@@ -23,12 +64,18 @@ $(document).ready(function initmap() {
     }, function () {
     })
         .done(function (response) {
-            $.each(response, function (id, location) {
-                var marker = L.marker(L.latLng(location.lat, location.lon));
+
+            $("#info").html(
+                "<div class='alert alert-info'> <strong>Cantidad de antenas del carrier seleccionado: </strong>" + response.totalAntennas + "</div>"
+            );
+
+            $.each(response.antennasData, function (id, data) {
+                var marker = L.marker(L.latLng(data.lat, data.lon)).on('mouseover', mouseOverEvent(data));
                 markerList.push(marker);
             });
             markerCluster.addLayers(markerList);
             map.addLayer(markerCluster);
+
         });
 });
 
@@ -44,8 +91,13 @@ $(document).ready(function () {
         }, function () {
         })
             .done(function (response) {
-                $.each(response, function (id, location) {
-                    var marker = L.marker(L.latLng(location.lat, location.lon));
+
+                $("#info").html(
+                    "<div class='alert alert-info'> <strong>Cantidad de antenas del carrier seleccionado: </strong>" + response.totalAntennas + "</div>"
+                );
+
+                $.each(response.antennasData, function (id, data) {
+                    var marker = L.marker(L.latLng(data.lat, data.lon)).on('mouseover', mouseOverEvent(data));
                     markerList.push(marker);
                 });
                 markerCluster.addLayers(markerList);
@@ -53,3 +105,4 @@ $(document).ready(function () {
             });
     });
 });
+
